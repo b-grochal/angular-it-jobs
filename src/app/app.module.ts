79d9d921e11cae02b1ea8259680dcaba6620cdb5
Bartosz <radu480@o2.pl>
2021-12-20 23:45:21 +0100

    Prepare general structure of directories.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { JobsPageComponent } from './pages/jobs-page/jobs-page.component';
+import { JobDetailsPageComponent } from './pages/job-details-page/job-details-page.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegistrationFormComponent } from './components/auth/registration-form/registration-form.component';
+import { JobComponent } from './components/jobs/job/job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    JobsPageComponent,
+    JobDetailsPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginFormComponent,
+    RegistrationFormComponent,
+    JobComponent
   ],
   imports: [
     BrowserModule,
