4ac590a7042da4e7e6586f76af4e8b8b765d2137
Bartosz <radu480@o2.pl>
2021-12-28 23:42:47 +0100

    Add basic routing between pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { JobDetailsPageComponent } from './pages/job-details-page/job-details-page.component';
+import { JobsPageComponent } from './pages/jobs-page/jobs-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'jobs', component: JobsPageComponent },
+  { path: 'jobs/:id', component: JobDetailsPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
