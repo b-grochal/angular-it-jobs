147ecd5cddfdb0cb1f63de9dd19e6c77199aa7f7
Bartosz <radu480@o2.pl>
2022-01-02 01:17:04 +0100

    Prepare login form component.

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe(
+        (result: any) => {
+          this.router.navigate(['home']);
+        },
+        (error: string) => {
+          console.log('Error');
+        }
+      );
+
+      alert('SUCCESS!! :-)');
+    }
+  }
 }
